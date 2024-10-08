bce94ee2a6ef4aa1e6bb045a3be7e5faba7fc760
ananyamisra5 <misraananya5@gmail.com>
2024-10-07 21:26:04 -0700

    Model.py stubbed out

--- src/model.py
+++ src/model.py
@@ -1,1 +1,14 @@
-#define pytorch model
+# Imported Pytorch Models
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+#A neural network class that inherits from nn.Module
+class FemnistModel(nn.Module):
+    def __init__(self):
+        
+
+    #Defines forward pass of the network
+    def forward(self, x):
+
+if __name__ == "__main__":
