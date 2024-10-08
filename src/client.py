c90ee8ef936c399393a456c49b4e2f57789ffde6
ananyamisra5 <misraananya5@gmail.com>
2024-10-07 22:19:57 -0700

    Client.py stubbed out

--- src/client.py
+++ src/client.py
@@ -1,1 +1,29 @@
-#implement flower clientgit pull
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import flwr as fl
+from model import FemnistModel
+
+class FemnistClient(fl.client.NumPyClient):
+    def __init__(self, model, train_loader, test_loader, device):
+        self.model = model
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.device = device
+
+    #def get_parameters():
+        
+
+    #def set_parameters():
+      
+
+    #def fit():
+       
+
+    #def evaluate():
+       
+
+#def train():
+   
+#def test():
+    
