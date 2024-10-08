f07ec93b9f7e809f17e7f67407537f4139be8277
ananyamisra5 <misraananya5@gmail.com>
2024-10-07 22:09:31 -0700

    Commenting init model.py

--- src/model.py
+++ src/model.py
@@ -12,9 +12,17 @@ class FemnistModel(nn.Module):
     def __init__(self):
         #Calls the parent constructor 
         super(FemnistModel, self).__init__()
+
+         # First convolutional layer: 1 input channel, 32 output channels, 3x3 kernel
         self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
+        
+         # Second convolutional layer: 32 input channels, 64 output channels, 3x3 kernel
         self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
+        
+         # First fully connected layer: 64 * 7 * 7 input features, 128 output feature
         self.fc1 = nn.Linear(in_features=64 * 7 * 7, out_features=128)
+        
+        # Output layer: 128 input features, 62 output features 
         self.fc2 = nn.Linear(in_features=128, out_features=62)
 
     def forward(self, x):
